94d8b0aa1400bc87921b491ce5d364bd6828f9eb
Mohamed Singano <singano2009@gmail.com>
2023-06-15 20:24:00 +0300

    fixed admin dashboard

--- admin/index.php
+++ admin/index.php
@@ -23,14 +23,13 @@
             <div class="flex justify-between">
                 <div class="p-1 mx-3 inline-flex items-center">
                     <i class="fas fa-bars pr-2 text-white" onclick="sidebarToggle()"></i>
-                    <h1 class="text-white p-2">Logo</h1>
+                    <h1 class="text-white p-2">LFMS</h1>
                 </div>
                 <div class="p-1 flex flex-row items-center">
-                    <a href="https://github.com/tailwindadmin/admin" class="text-white p-2 mr-2 no-underline hidden md:block lg:block">Github</a>
 
 
-                    <img onclick="profileToggle()" class="inline-block h-8 w-8 rounded-full" src="https://avatars0.githubusercontent.com/u/4323180?s=460&v=4" alt="">
-                    <a href="#" onclick="profileToggle()" class="text-white p-2 no-underline hidden md:block lg:block">Adam Wathan</a>
+                    <img onclick="profileToggle()" class="inline-block h-8 w-8 rounded-full" src="dist/images/download.php@1x_1.jpg" alt="">
+                    <a href="#" onclick="profileToggle()" class="text-white p-2 no-underline hidden md:block lg:block">System Admin</a>
                     <div id="ProfileDropDown" class="rounded hidden shadow-md bg-white absolute pin-t mt-12 mr-1 pin-r">
                         <ul class="list-reset">
                           <li><a href="#" class="no-underline px-4 py-2 block text-black hover:bg-grey-light">My account</a></li>
@@ -50,81 +49,23 @@
 
                 <ul class="list-reset flex flex-col">
                     <li class=" w-full h-full py-3 px-2 border-b border-light-border bg-white">
-                        <a href="index.html"
+                        <a href="index.php"
                            class="font-sans font-hairline hover:font-normal text-sm text-nav-item no-underline">
                             <i class="fas fa-tachometer-alt float-left mx-2"></i>
                             Dashboard
                             <span><i class="fas fa-angle-right float-right"></i></span>
                         </a>
                     </li>
+                    
                     <li class="w-full h-full py-3 px-2 border-b border-light-border">
-                        <a href="forms.html"
-                           class="font-sans font-hairline hover:font-normal text-sm text-nav-item no-underline">
-                            <i class="fab fa-wpforms float-left mx-2"></i>
-                            Forms
-                            <span><i class="fa fa-angle-right float-right"></i></span>
-                        </a>
-                    </li>
-                    <li class="w-full h-full py-3 px-2 border-b border-light-border">
-                        <a href="buttons.html"
-                           class="font-sans font-hairline hover:font-normal text-sm text-nav-item no-underline">
-                            <i class="fas fa-grip-horizontal float-left mx-2"></i>
-                            Buttons
-                            <span><i class="fa fa-angle-right float-right"></i></span>
-                        </a>
-                    </li>
-                    <li class="w-full h-full py-3 px-2 border-b border-light-border">
-                        <a href="tables.php"
+                        <a href="items.php"
                            class="font-sans font-hairline hover:font-normal text-sm text-nav-item no-underline">
                             <i class="fas fa-table float-left mx-2"></i>
-                            Tables
+                            Items
                             <span><i class="fa fa-angle-right float-right"></i></span>
                         </a>
                     </li>
-                    <li class="w-full h-full py-3 px-2 border-b border-light-border">
-                        <a href="ui.html"
-                           class="font-sans font-hairline hover:font-normal text-sm text-nav-item no-underline">
-                            <i class="fab fa-uikit float-left mx-2"></i>
-                            Ui components
-                            <span><i class="fa fa-angle-right float-right"></i></span>
-                        </a>
-                    </li>
-                    <li class="w-full h-full py-3 px-2 border-b border-300-border">
-                        <a href="modals.html" class="font-sans font-hairline hover:font-normal text-sm text-nav-item no-underline">
-                            <i class="fas fa-square-full float-left mx-2"></i>
-                            Modals
-                            <span><i class="fa fa-angle-right float-right"></i></span>
-                        </a>
-                    </li>
-                    <li class="w-full h-full py-3 px-2">
-                        <a href="#"
-                           class="font-sans font-hairline hover:font-normal text-sm text-nav-item no-underline">
-                            <i class="far fa-file float-left mx-2"></i>
-                            Pages
-                            <span><i class="fa fa-angle-down float-right"></i></span>
-                        </a>
-                        <ul class="list-reset -mx-2 bg-white-medium-dark">
-                            <li class="border-t mt-2 border-light-border w-full h-full px-2 py-3">
-                                <a href="login.html"
-                                   class="mx-4 font-sans font-hairline hover:font-normal text-sm text-nav-item no-underline">
-                                    Login Page
-                                    <span><i class="fa fa-angle-right float-right"></i></span>
-                                </a>
-                            </li>
-                            <li class="border-t border-light-border w-full h-full px-2 py-3">
-                                <a href="register.html"
-                                   class="mx-4 font-sans font-hairline hover:font-normal text-sm text-nav-item no-underline">
-                                    Register Page
-                                    <span><i class="fa fa-angle-right float-right"></i></span>
-                                </a>
-                            </li>
-                            <li class="border-t border-light-border w-full h-full px-2 py-3">
-                                <a href="404.html"
-                                   class="mx-4 font-sans font-hairline hover:font-normal text-sm text-nav-item no-underline">
-                                    404 Page
-                                    <span><i class="fa fa-angle-right float-right"></i></span>
-                                </a>
-                            </li>
+                    
                         </ul>
                     </li>
                 </ul>
@@ -311,116 +252,9 @@
                     <!-- /Cards Section Ends Here -->
 
                     <!-- Progress Bar -->
-                    <div class="flex flex-1 flex-col md:flex-row lg:flex-row mx-2 mt-2">
-                        <div class="rounded overflow-hidden shadow bg-white mx-2 w-full pt-2">
-                            <div class="px-6 py-2 border-b border-light-grey">
-                                <div class="font-bold text-xl">Progress Among Projects</div>
-                            </div>
-                            <div class="">
-                                <div class="w-full">
-
-                                    <div class="shadow w-full bg-grey-light">
-                                        <div class="bg-blue-500 text-xs leading-none py-1 text-center text-white"
-                                             style="width: 45%">45%
-                                        </div>
-                                    </div>
-
-
-                                    <div class="shadow w-full bg-grey-light mt-2">
-                                        <div class="bg-teal-500 text-xs leading-none py-1 text-center text-white"
-                                             style="width: 55%">55%
-                                        </div>
-                                    </div>
-
-
-                                    <div class="shadow w-full bg-grey-light mt-2">
-                                        <div class="bg-orange-500 text-xs leading-none py-1 text-center text-white"
-                                             style="width: 65%">65%
-                                        </div>
-                                    </div>
-
-
-                                    <div class="shadow w-full bg-grey-300 mt-2">
-                                        <div class="bg-red-800 text-xs leading-none py-1 text-center text-white"
-                                             style="width: 75%">75%
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+                   
                     <!--Profile Tabs-->
-                    <div class="flex flex-1 flex-col md:flex-row lg:flex-row mx-2 p-1 mt-2 mx-auto lg:mx-2 md:mx-2 justify-between">
-                        <!--Top user 1-->
-                        <div class="rounded rounded-t-lg overflow-hidden shadow max-w-xs my-3">
-                            <img src="https://i.imgur.com/w1Bdydo.jpg" alt="" class="w-full"/>
-                            <div class="flex justify-center -mt-8">
-                                <img src="https://i.imgur.com/8Km9tLL.jpg" alt=""
-                                     class="rounded-full border-solid border-white border-2 -mt-3">
-                            </div>
-                            <div class="text-center px-3 pb-6 pt-2">
-                                <h3 class="text-black text-sm bold font-sans">Olivia Dunham</h3>
-                                <p class="mt-2 font-sans font-light text-grey-700">Hello, i'm from another the other
-                                    side!</p>
-                            </div>
-                            <div class="flex justify-center pb-3 text-grey-dark">
-                                <div class="text-center mr-3 border-r pr-3">
-                                    <h2>34</h2>
-                                    <span>Photos</span>
-                                </div>
-                                <div class="text-center">
-                                    <h2>42</h2>
-                                    <span>Friends</span>
-                                </div>
-                            </div>
-                        </div>
-                        <!--Top user 2-->
-                        <div class="rounded rounded-t-lg overflow-hidden shadow max-w-xs my-3">
-                            <img src="https://i.imgur.com/w1Bdydo.jpg" alt="" class="w-full"/>
-                            <div class="flex justify-center -mt-8">
-                                <img src="https://i.imgur.com/8Km9tLL.jpg" alt=""
-                                     class="rounded-full border-solid border-white border-2 -mt-3">
-                            </div>
-                            <div class="text-center px-3 pb-6 pt-2">
-                                <h3 class="text-black text-sm bold font-sans">Olivia Dunham</h3>
-                                <p class="mt-2 font-sans font-light text-grey-dark">Hello, i'm from another the other
-                                    side!</p>
-                            </div>
-                            <div class="flex justify-center pb-3 text-grey-dark">
-                                <div class="text-center mr-3 border-r pr-3">
-                                    <h2>34</h2>
-                                    <span>Photos</span>
-                                </div>
-                                <div class="text-center">
-                                    <h2>42</h2>
-                                    <span>Friends</span>
-                                </div>
-                            </div>
-                        </div>
-                        <!--Top user 3-->
-                        <div class="rounded rounded-t-lg overflow-hidden shadow max-w-xs my-3">
-                            <img src="https://i.imgur.com/w1Bdydo.jpg" alt="" class="w-full"/>
-                            <div class="flex justify-center -mt-8">
-                                <img src="https://i.imgur.com/8Km9tLL.jpg" alt=""
-                                     class="rounded-full border-solid border-white border-2 -mt-3">
-                            </div>
-                            <div class="text-center px-3 pb-6 pt-2">
-                                <h3 class="text-black text-sm bold font-sans">Olivia Dunham</h3>
-                                <p class="mt-2 font-sans font-light text-grey-dark">Hello, i'm from another the other
-                                    side!</p>
-                            </div>
-                            <div class="flex justify-center pb-3 text-grey-dark">
-                                <div class="text-center mr-3 border-r pr-3">
-                                    <h2>34</h2>
-                                    <span>Photos</span>
-                                </div>
-                                <div class="text-center">
-                                    <h2>42</h2>
-                                    <span>Friends</span>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+                    
                     <!--/Profile Tabs-->
                 </div>
             </main>
