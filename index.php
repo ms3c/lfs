7b327500afd16157c68cafadf4cf4ec39a979bd9
Mohamed Singano <singano2009@gmail.com>
2023-06-15 20:11:21 +0300

    Update index.php

--- index.php
+++ index.php
@@ -8,8 +8,7 @@ session_start();
     <meta charset="utf-8">
     <title>Lost found Systems</title>
     <meta content="width=device-width, initial-scale=1.0" name="viewport">
-    <meta content="Free HTML Templates" name="keywords">
-    <meta content="Free HTML Templates" name="description">
+
 
     <!-- Favicon -->
     <link href="img/favicon.ico" rel="icon">
