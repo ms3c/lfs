32bf763ea9e7124d4bdc48cba7cae0411d801535
Mohamed Singano <singano2009@gmail.com>
2023-06-17 00:00:13 +0300

    Update handler.php

--- helpers/handler.php
+++ helpers/handler.php
@@ -30,10 +30,8 @@ $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
 if(isset($_POST["submit"])) {
   $check = getimagesize($_FILES["photo"]["tmp_name"]);
   if($check !== false) {
-    echo "File is an image - " . $check["mime"] . ".";
     $uploadOk = 1;
   } else {
-    echo "File is not an image.";
     $uploadOk = 0;
   }
 }
@@ -46,26 +44,21 @@ if (file_exists($target_file)) {
 
 // Check file size
 if ($_FILES["photo"]["size"] > 500000) {
-  echo "Sorry, your file is too large.";
   $uploadOk = 0;
 }
 
 // Allow certain file formats
 if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
 && $imageFileType != "gif" ) {
-  echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
   $uploadOk = 0;
 }
 
 // Check if $uploadOk is set to 0 by an error
 if ($uploadOk == 0) {
-  echo "Sorry, your file was not uploaded.";
 // if everything is ok, try to upload file
 } else {
   if (move_uploaded_file($_FILES["photo"]["tmp_name"], $target_file)) {
-    echo "The file ". htmlspecialchars( basename( $_FILES["photo"]["name"])). " has been uploaded.";
   } else {
-    echo "Sorry, there was an error uploading your file.";
   }
 }
 
