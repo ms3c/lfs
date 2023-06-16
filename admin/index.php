0d7fb3dfb7dee8daa30a5361ac61336cdae0b2e2
Mohamed Singano <singano2009@gmail.com>
2023-06-16 10:34:14 +0300

    Update index.php

--- admin/index.php
+++ admin/index.php
@@ -1,14 +1,6 @@
 <!DOCTYPE html>
 <?php 
 session_start();
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
-
-if($_SESSION['role'] != '1'){
-    header("Location: ../login.php?error=notadmin");
-    exit();
-}
 ?>
 <html lang="en">
 
