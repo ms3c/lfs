5fea1def017c5d8616b149b80ec66f83b6a293ea
Mohamed Singano <singano2009@gmail.com>
2023-06-16 10:30:15 +0300

    Update index.php

--- admin/index.php
+++ admin/index.php
@@ -1,6 +1,9 @@
 <!DOCTYPE html>
 <?php 
 session_start();
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 if(!isset($_SESSION['role'])){
     header("Location: ../login.php");
     exit();
